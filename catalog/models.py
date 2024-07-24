c25cb6b37fbf4fb9bd3ddd6211f6544b703f149d
Pavlukhina Ksenia <kseny100297@yandex.ru>
2024-07-24 21:45:06 +0500

    delete migrate

--- catalog/models.py
+++ catalog/models.py
@@ -55,12 +55,7 @@ class Product(models.Model):
     )
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    manufactured_at = models.DateField(
-        verbose_name='Дата производства',
-        help_text='Введите дату производства продукта',
-        null=True,
-        blank=True
-    )
+
     def __str__(self):
         return f"{self.name} {self.description}"
 
